a64c9f5a080346e92c2f7a2e06fc15ba19dfbd9e
houhuizhao <1004855765@qq.com>
2016-11-25 15:21:50 +0800

    个人主页头像设置

---
+++ app/src/main/java/com/android/trail/xizheng/PersonalActivity.java
@@ -0,0 +1,17 @@
+package com.android.trail.xizheng;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.android.trail.R;
+
+/**
+ * Created by Lenovo on 2016/11/25.
+ */
+
+public class PersonalActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personalipd);
+    }
+}
