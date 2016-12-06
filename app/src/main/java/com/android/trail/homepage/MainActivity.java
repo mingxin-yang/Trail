cad01a2f2c0266b981c37216f56d5e0357e693ee
anjingQL <270770019@qq.com>
2016-12-06 17:12:27 +0800

    Merge branch 'master' of D:\shixun\Trail with conflicts.

--- app/src/main/java/com/android/trail/homepage/MainActivity.java
+++ app/src/main/java/com/android/trail/homepage/MainActivity.java
@@ -11,13 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import com.android.trail.R;
 import com.android.trail.com.adroid.huizhao.ViewPager.JieShaoActivity;
-import com.android.trail.hongxue.Discuss;
-import com.android.trail.wangyang.BusStopActivity;
-import com.android.trail.xizheng.PersonalActivity;
 import com.android.trail.zhenfeng.FJTabview;
+import com.android.trail.xizheng.PersonalActivity;
 
 
 public class MainActivity extends Activity {
