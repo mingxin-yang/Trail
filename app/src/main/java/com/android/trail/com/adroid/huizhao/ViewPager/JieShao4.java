4dcb2fc92129d23701f295541b56a8c94e010d88
wupengp <191799350@qq.com>
2016-12-13 14:31:19 +0800

    介绍界面的修改

--- app/src/main/java/com/android/trail/com/adroid/huizhao/ViewPager/JieShao4.java
+++ app/src/main/java/com/android/trail/com/adroid/huizhao/ViewPager/JieShao4.java
@@ -1,9 +1,9 @@
 package com.android.trail.com.adroid.huizhao.ViewPager;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,25 +11,22 @@ import com.android.trail.R;
 
 import qiu.niorgai.StatusBarCompat;
 
-/**
- * Created by Lenovo on 2016/12/1.
- */
-
-public class JieShao3 extends Activity {
+public class JieShao4 extends AppCompatActivity {
 
     private Button back;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.jie_shao_3);
+        setContentView(R.layout.activity_jie_shao4);
         StatusBarCompat.setStatusBarColor(this, Color.BLUE,255);
-        back = (Button)findViewById(R.id.jshao3_back);
+
+        back = (Button)findViewById(R.id.jshao4_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent back = new Intent();
-                back.setClass(JieShao3.this,JieShaoActivity.class);
+                back.setClass(JieShao4.this,JieShaoActivity.class);
                 startActivity(back);
                 finish();
             }
