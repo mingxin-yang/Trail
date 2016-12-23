b4fa9dcbc13fedbcb017e7cdf2392ede80743b6f
anjingQL <270770019@qq.com>
2016-12-23 22:34:21 +0800

    公交页按钮所对应的跳转

---
+++ app/src/main/java/com/android/trail/wangyang/HCZActivity.java
@@ -0,0 +1,15 @@
+package com.android.trail.wangyang;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.trail.R;
+
+public class HCZActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_hcz );
+    }
+}
